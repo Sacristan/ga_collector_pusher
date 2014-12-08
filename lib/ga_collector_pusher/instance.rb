61264e1996418018de2488f9bea7ef94d93c9ced
Sacristan <girts.kesteris@gmail.com>
2014-12-08 11:38:29 +0200

    Some darn tests

--- lib/ga_collector_pusher/instance.rb
+++ lib/ga_collector_pusher/instance.rb
@@ -50,6 +50,7 @@ module GACollectorPusher
 
     private
       def send_to_ga
+        puts GOOGLE_ANALYTICS_SETTINGS
         puts @params.inspect
         params = @params.stringify_keys
         RestClient.get 'https://www.google-analytics.com/collect', params: params, timeout: self.timeout, open_timeout: self.open_timeout
--- spec/config.rb
+++ spec/config.rb
@@ -1,5 +1,5 @@
 GOOGLE_ANALYTICS_SETTINGS = {
   version: "1",
-  tracking_code: "UA-41298960-5",
-  domain: "app.stockholmhealth.com"
+  tracking_code: "test",
+  domain: "test.test.test"
 }
